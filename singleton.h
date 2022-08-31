a045890805c92991c7f58c02916f69ac3523d37a
adayswait <adayswait@139.com>
2022-08-31 11:25:25 +0800

    Create singleton.h

---
+++ singleton.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template<class T>
+class Singleton
+{
+public:
+    Singleton(Singleton const&) = delete;
+    Singleton& operator=(Singleton const&) = delete;
+
+    static T& Instance()
+    {
+        static T inst;
+        return inst;
+    }
+
+protected:
+	Singleton() = default;
+	~Singleton() = default;
+};
+
